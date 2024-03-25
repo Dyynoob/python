05a348573e2f0405eea017bc53adbd9ab8971f16
Dyynoob <82898420+Dyynoob@users.noreply.github.com>
2024-03-25 17:02:17 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,13 @@
-print("Welcome to the ASMOS python script.")
-
 def delay(seconds):
     start_time = 0
     for _ in range(int(100000000 * seconds)):
         start_time += 0.00000001
+
+
+
+
+
+print("Welcome to the ASMOS python script.")
+delay(3)
+print("WAIT 3 SECONDS!")
+
